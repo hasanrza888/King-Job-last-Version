79ce87ea7eca4bd1f647b8a22b7088815b68bc3d
Shikhkarim <71795994+memmedov-karim@users.noreply.github.com>
2024-01-27 13:24:25 +0400

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
 export const localapiUrl = 'http://localhost:5000';
-export const deployedapiUrl = 'https://seal-app-qdwqo.ondigitalocean.app'
+export const deployedapiUrl = 'https://clownfish-app-t2clr.ondigitalocean.app/'

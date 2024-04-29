b51261c8b31ea6225f3e8046e1106242e0c05f4a
Shikhkarim <71795994+memmedov-karim@users.noreply.github.com>
2024-04-29 12:54:04 -0700

    Update axiosInstances.js

--- src/services/axiosInstances.js
+++ src/services/axiosInstances.js
@@ -1,5 +1,3 @@
-//hostedEndpoint:https://seal-app-qdwqo.ondigitalocean.app/
-//localhostedEndpoint:http://localhost:5000/
 import { localapiUrl,deployedapiUrl } from '../constants/constants';
 import axios from 'axios';
 const instance = axios.create({

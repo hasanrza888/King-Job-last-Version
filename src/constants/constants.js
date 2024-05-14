dc9680089846dead7482712800e2479854b039ca
Shikhkarim <71795994+memmedov-karim@users.noreply.github.com>
2024-05-13 23:29:37 -0700

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
 export const localapiUrl = 'http://localhost:5000';
-export const deployedapiUrl = 'https://seahorse-app-aunwl.ondigitalocean.app'
+export const deployedapiUrl = 'https://dolphin-app-hgxpz.ondigitalocean.app'
